a974e79da5bcbbd6de04f0cd46fbac05a88a54c3
mightydread <mightydreadmario@gmail.com>
2014-05-29 17:04:17 +0200

    Neue lager

--- lager/lieferung.php
+++ lager/lieferung.php
@@ -2,12 +2,12 @@
 <!DOCTYPE html>
 
 
-<html lang="">
+<html lang="de">
 
     <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8mb4" />
+
         <title>Lager Lieferung</title>
-        <link rel="stylesheet" type="text/css" href="../media/global.css">
+        <link rel="stylesheet" type="text/css" href="/media/global.css">
         <link rel="stylesheet" type="text/css" href="/media/lager.css">
         <?php require $_SERVER["DOCUMENT_ROOT"].'/includes/lager.php' ?>
     </head>
@@ -22,19 +22,19 @@
         <div class="wrap">
             <?php
 if (isset($_POST['datum'])) {
-    foreach (waren(kasten) as $typ) { liefer_row($typ,kasten); }
-    foreach (waren(flasche) as $typ) { liefer_row($typ,flasche); }
+    echo "<div class=legend_row><div class=name>Kastenware</div><div class=anzahl>Kasten</div><div class=save></div></div>";
+    foreach (waren(kasten) as $typ) { liefer_row($typ); }
+    echo "<div class=legend_row><div class=name>Flaschenware</div><div class=anzahl>volle Flaschen</div><div class=save></div></div>";
+    foreach (waren(flasche) as $typ) { liefer_row($typ); }
 }
 else {
+    echo "Bitte Datum der nächsten Schicht nehmen!";
     echo "<form id=datum method=post action=\"".$_SERVER["PHP_SELF"]."\">";
     echo "<input type=date name=datum >";
     echo "<input type=submit >";
     echo "</form>";
 }
             ?>
-        </div>
-        <div id=debug>
-            <?php debug(); ?>
         </div>
     </body>
 
