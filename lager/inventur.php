6c3cb63a412358c4f6f024d09201b4f032b5ca24
mightydread <mightydreadmario@gmail.com>
2014-06-18 14:19:40 +0200

    remove debugging

--- lager/inventur.php
+++ lager/inventur.php
@@ -16,7 +16,7 @@
             foreach (waren() as $typ) {
                 if (!isset($_SESSION[$typ])) {$_SESSION[$typ]=array("i_g"=>"0","i_k"=>"0","st"=>info($typ)['st'],"art"=>info($typ)['art'],"preis"=>info($typ)['preis']);}
                 if (!isset($_SESSION[$typ][st]) or !isset($_SESSION[$typ][art]) or !isset($_SESSION[$typ][preis])) {$_SESSION[$typ]=array("st"=>info($typ)['st'],"art"=>info($typ)['art'],"preis"=>info($typ)['preis']);}
-                print_r($_SESSION);
+               
                 if (isset($_POST[$typ])) {
                     $_SESSION[$typ]['i_g']=$_POST[$typ._g];
                     $_SESSION[$typ]['i_k']=$_POST[$typ._k];
