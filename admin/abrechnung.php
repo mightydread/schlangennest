1e283dc1b0c9750dd07cf6467b91ad927aa09b83
mightydread <mightydreadmario@gmail.com>
2014-09-19 01:37:55 +0200

    event abgerechnet check

--- admin/abrechnung.php
+++ admin/abrechnung.php
@@ -20,9 +20,14 @@
             <li class="va_umsatz">Umsatz</li>
 
         </ul> 
-        <?php foreach (all_va() as $key => $value) { ?>
+        <?php foreach (all_va() as $key => $value) {
+        $sql2 = "SELECT 1 FROM abrechnung WHERE datum ='".$value['datum']."' LIMIT 1";
+                $result2 = mysqli_query($con,$sql2);
+                if (mysqli_fetch_row($result2)) { 
+                }
+                else { ?>
         <ul class="va_row">
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>#umsatz" >
                             <input type="hidden" name=datum value="<?php echo $value['datum']; ?>">
             <li class="va_datum"><?php echo date('d.m.y', strtotime($value['datum'])); ?></li>
             <li class="va_name"><?php echo $value['name']; ?></li>
@@ -31,7 +36,7 @@
             
                         </form>
         </ul> 
-        <?php } ?>
+        <?php } }?>
         <form class="va_row" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" id="new">
             <input class="va_datum" type="date" name="datum" >
             <input class="va_name" type="text" name="name" >
